71eab9a082667e1ed98663677c7ac03b2fb00f8b
ziming <martiont@163.com>
2019-04-18 16:35:14 +0800

    原始版本spring

--- src/main/java/cn/ziming/my/shop/commons/context/SpringContext.java
+++ src/main/java/cn/ziming/my/shop/commons/context/SpringContext.java
@@ -1,10 +1,14 @@
 package cn.ziming.my.shop.commons.context;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public final class SpringContext {
+    private static final Logger logger = LoggerFactory.getLogger(SpringContext.class);
     public Object getSpringBean(String beanId) {
+
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
         return context.getBean(beanId);
     }
